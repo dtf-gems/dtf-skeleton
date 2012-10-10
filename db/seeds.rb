5605014e4616a222f81ab7a98877c052b26a02b9
David Deryl Downey <me@daviddwdowney.com>
2012-10-10 16:45:56 -0400

    Added db/seeds.rb file, and modified to notify the user to mod its require and remove the abort line. This was moved from dtf to here.

---
+++ db/seeds.rb
@@ -0,0 +1,6 @@
+# encoding: UTF-8
+require 'dtf-skeleton'
+puts "Remember to change 'dtf-skeleton' to the name of the gem, and remove this and the abort() line"
+abort()
+
+User.create(full_name: "Library Owner", email_address: "library_owner@localhost.lan", user_name: "library_owner")
