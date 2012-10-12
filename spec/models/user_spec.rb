f34623ec68cbc09767a08c04ba4a5ee9b0c762eb
David Deryl Downey <me@daviddwdowney.com>
2012-10-12 07:03:14 -0400

    Refactored model specs to be cleaner.

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -6,51 +6,39 @@ describe "User" do
 
   context "when instantiated" do
 
-    let(:user) { User.new }
-    
     it "should be the proper class" do
-      user.should be_a(User)      
+      Fabricate.build(:user).should be_a(User)
     end
 
-    it "should be invalid without a user_name" do    
-      user.should_not be_valid
-      user.errors.should_not be_empty
-      user.errors.messages[:user_name].should eq(["can't be blank"])
-      user.new_record?.should be_true
-    end  
-  
-    it "should be invalid without an email_address" do    
-      user.should_not be_valid
-      user.errors.should_not be_empty
-      user.errors.messages[:email_address].should eq(["can't be blank"])
-      user.new_record?.should be_true
+    it "should be invalid without a user_name" do
+      Fabricate.build(:user, user_name: nil).should_not be_valid
     end
-    
-    it "should be invalid without a full_name" do    
-      user.should_not be_valid
-      user.errors.should_not be_empty
-      user.errors.messages[:full_name].should eq(["can't be blank"])
-      user.new_record?.should be_true
+
+    it "should be invalid without an email_address" do
+      Fabricate.build(:user, email_address: nil).should_not be_valid
+    end
+
+    it "should be invalid without a full_name" do
+      Fabricate.build(:user, full_name: nil).should_not be_valid
     end
 
     it "should not be saved" do
-      user.new_record?.should be_true
-      user.persisted?.should_not be_true
+      Fabricate.build(:user).new_record?.should be_true
     end
 
-  end  
-  
+  end
+
   context "when created" do
     let(:user) { Fabricate(:user)}
-    
-    it "should have a valid user_name, full_name, and email_address" do    
+
+    it "should have a valid user_name, full_name, and email_address" do
       user.should be_valid
       user.errors.should be_empty
       user.user_name.should_not be_nil
       user.full_name.should_not be_nil
       user.email_address.should_not be_nil
-    end  
-  
+    end
+
     it "should be saved" do
       user.new_record?.should_not be_true
       user.persisted?.should be_true
